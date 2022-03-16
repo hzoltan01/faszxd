a4d2eec465443f53870eb2b991fa297c6d039a23
Zóli <80711267+hzoltan01@users.noreply.github.com>
2022-03-16 14:46:53 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace faszxd
+{
+    class Program {
+        private static void Main(string[] args) {
+            for (int i = 0; i < 1000000; ++i) {
+                Console.WriteLine("fasz XD");
+            }
+        }
+    }
+}
