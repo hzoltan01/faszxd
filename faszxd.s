cf08cdbe12435050ddd10ded067ed07641fb8060
DavidHelmeczi <59920623+DavidHelmeczi@users.noreply.github.com>
2023-09-22 11:37:14 +0200

    Update faszxd.s

--- faszxd.s
+++ faszxd.s
@@ -1,25 +1,25 @@
- .MODEL SMALL
-.STACK 100H
-.DATA
+section .data
+    message db "FaszXD", 0  ; Null-terminated string
 
-M1  DB  'Fasz XD',0DH,0AH,'$'
-.CODE
-MAIN PROC
-MOV AX,@DATA
-MOV DS,AX
+section .text
+global _start
 
-MOV CX,1000000
-PRINT_LOOP:
+_start:
+    mov ecx, 1000000       ; Set the loop counter to 1,000,000
 
-MOV AH,9
-LEA DX,M1
-INT 21H
-DEC CX
+loop_start:
+    mov eax, 4             ; Syscall number for sys_write (stdout)
+    mov ebx, 1             ; File descriptor for stdout
+    mov edx, 6             ; Length of the string
+    lea ecx, [message]     ; Load the address of the string into ecx
+    int 0x80               ; Call the kernel to print the string
 
-JNZ PRINT_LOOP
+    dec ecx                ; Decrement the loop counter
+    jnz loop_start         ; Jump back to loop_start if not zero
 
-MOV AH,4CH
-INT 21H
+    ; Exit the program
+    mov eax, 1             ; Syscall number for sys_exit
+    xor ebx, ebx           ; Exit status code (0)
+    int 0x80               ; Call the kernel to exit
 
-MAIN ENDP
-END MAIN
+section .bss
